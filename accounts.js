22c7731a1f9de1aa56e093fb015c6fd4d18c9880
Cach-22 <chipscael@gmail.com>
2026-05-26 22:04:04 -0600

    Update accounts.js

--- accounts.js
+++ accounts.js
@@ -1,10 +1,19 @@
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-auth.js";
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider
+} from "https://www.gstatic.com/firebasejs/12.12.0/firebase-auth.js";
 
-const auth = getAuth();
+const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
-async function login() {
+window.login = async function() {
+  try {
     const result = await signInWithPopup(auth, provider);
 
     alert("Logged in as: " + result.user.displayName);
+
+  } catch(err) {
+    alert(err.message);
+  }
 }
