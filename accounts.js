bdd6fe0f5e219f43928079d3a25882c7bdc6156f
Cach-22 <chipscael@gmail.com>
2026-05-26 22:30:57 -0600

    Update accounts.js

--- accounts.js
+++ accounts.js
@@ -1,15 +1,19 @@
-function login() {
+window.login = function () {
   const clientId = "1509046885682249858";
 
   const redirectUri = encodeURIComponent("http://192.168.1.115:8000/");
 
-  window.location.href =
+  const url =
     "https://discord.com/oauth2/authorize" +
     `?client_id=${clientId}` +
-    "&response_type=token" +
+    "&response_type=code" +
     `&redirect_uri=${redirectUri}` +
     "&scope=identify%20email`;
-}
+
+  console.log("Redirecting to:", url); // sanity check
+
+  window.location.href = url;
+};
 
 const hash = new URLSearchParams(window.location.hash.substring(1));
 const token = hash.get("access_token");
