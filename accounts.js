e65933c1a76699644558357b0b3f78c1fcff0d1a
Cach-22 <chipscael@gmail.com>
2026-05-27 17:39:37 -0600

    Update accounts.js

--- accounts.js
+++ accounts.js
@@ -1,6 +1,6 @@
 function login() {
-  alert("trying");
-
+  alert("logging in")
+  
   const clientId = "1509046885682249858";
   const redirectUri = encodeURIComponent("http://192.168.1.115:8000/");
 
@@ -18,39 +18,14 @@ const hash = new URLSearchParams(window.location.hash.substring(1));
 const token = hash.get("access_token");
 
 if (token) {
-
   fetch("https://discord.com/api/users/@me", {
     headers: {
       Authorization: `Bearer ${token}`
     }
   })
-
-  .then(res => res.json())
-
-  .then(user => {
-
-    console.log(user);
-
-    const avatar =
-      `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`;
-
-    document.getElementById("accountBox").innerHTML = `
-      <img src="${avatar}"
-           width="90"
-           style="border-radius:50%;">
-
-      <h3>${user.global_name || user.username}</h3>
-
-      <p>@${user.username}</p>
-
-      <p>ID: ${user.id}</p>
-
-      <button onclick="logout()">Logout</button>
-    `;
-  });
-}
-
-function logout() {
-  window.location.hash = "";
-  location.reload();
+    .then(res => res.json())
+    .then(user => {
+      console.log(user);
+      alert("Logged in as " + user.username);
+    });
 }
