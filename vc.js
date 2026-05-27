9ca80f1381d1acc79613fa0784466c1e4089faf3
Cach-22 <chipscael@gmail.com>
2026-05-26 21:54:13 -0600

    Update vc.js

--- vc.js
+++ vc.js
@@ -1,24 +1,11 @@
-let localStream;
-
 async function startMic() {
   try {
-    localStream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    const stream = await navigator.mediaDevices.getUserMedia({
+      audio: true
+    });
+
     alert("Mic enabled");
-
-    // optional: test audio playback (self echo)
-    const audio = document.createElement("audio");
-    audio.srcObject = localStream;
-    audio.autoplay = true;
-    document.body.appendChild(audio);
-
-  } catch (err) {
+  } catch(err) {
     alert(err);
   }
 }
-
-const stream = localStream
-const pc = new RTCPeerConnection({
-  iceServers: [{ urls: "stun:stun.l.google.com:19302" }]
-});
-
-stream.getTracks().forEach(track => pc.addTrack(track, stream));
