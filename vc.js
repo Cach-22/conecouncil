09b0348216019b26952fbbdd53d8c49dea9e116c
Cach-22 <chipscael@gmail.com>
2026-05-26 21:55:43 -0600

    Update vc.js

--- vc.js
+++ vc.js
@@ -1,11 +1,18 @@
 async function startMic() {
   try {
+
+    if (!navigator.mediaDevices) {
+      alert("mediaDevices not available");
+      return;
+    }
+
     const stream = await navigator.mediaDevices.getUserMedia({
       audio: true
     });
 
     alert("Mic enabled");
+
   } catch(err) {
-    alert(err);
+    alert(err.message);
   }
 }
