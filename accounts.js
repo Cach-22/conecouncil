29088bf96bb762486233119631bda32e64508d36
Cach-22 <chipscael@gmail.com>
2026-05-26 22:10:41 -0600

    Update accounts.js

--- accounts.js
+++ accounts.js
@@ -1,19 +1,32 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-app.js";
+
 import {
   getAuth,
   signInWithPopup,
   GoogleAuthProvider
 } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-auth.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBb7jFUxQZoyZEypndvRv7rDYfwNHhfcrg",
+  authDomain: "theconecouncil.firebaseapp.com",
+  projectId: "theconecouncil",
+  storageBucket: "theconecouncil.firebasestorage.app",
+  messagingSenderId: "470783433143",
+  appId: "1:470783433143:web:4d887484c6c7acab6b7032"
+};
+
+const app = initializeApp(firebaseConfig);
+
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
-window.login = async function() {
+window.login = async function () {
   try {
     const result = await signInWithPopup(auth, provider);
 
     alert("Logged in as: " + result.user.displayName);
 
   } catch(err) {
-    alert(err);
+    alert(err.message);
   }
-}
+};
